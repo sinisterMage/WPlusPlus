1c235d986313e7e5ec5e3c06f54b8c0c6c2cc978
ofekb <ofek.bickel@gmail.com>
2025-08-02 09:09:19 +0300

    fixed vercel SSR

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,7 +1,11 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react'; // ✅ import the integration
+import vercel from '@astrojs/vercel/serverless'; // 👈 enable SSR
+
 
 export default defineConfig({
+  output: 'server', // required for server routes like /api/*
+  adapter: vercel(),
   integrations: [react()], // ✅ enable it here
 });
